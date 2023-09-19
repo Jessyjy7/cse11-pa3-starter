741000aa4c88d6c79520b894033cf5d0a5a3b9c9
Jessyjy7 <jiy018@ucsd.edu>
2023-09-19 19:35:48 +0800

    drill1&2

---
+++ Drill1.java
@@ -0,0 +1,15 @@
+class A {
+    int f1;
+    String f2;
+  }
+  
+class B {
+    A field1;
+    String field2;
+  }
+
+class C {
+    A fieldA;
+    B fieldB;
+    int field3;
+  }
---
+++ Drill2.java
@@ -0,0 +1,18 @@
+class C1 {
+    C2 other;
+    C1(C2 other) {
+      this.other = other;
+    }
+  }
+  
+class C2 {
+    int x;
+    C2(int x) {
+      this.x = x;
+    }
+  }
+
+class Drill2 {
+    C2 field;
+    
+}
